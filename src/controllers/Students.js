e3d28993d1c34712e41577c44e052a75b965ad9c
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-07-19 18:50:03 -0300

    Metodo de atualização

--- src/controllers/Students.js
+++ src/controllers/Students.js
@@ -13,5 +13,19 @@ module.exports = {
         return res.status(200).json({id});
     },
     
-
+    update: async (req,res) => {
+        const {registration,quiz_finished,questions_rights,questions_wrongs} = req.body;
+        const student = await connection('students').where({registration}).select().first();
+        const qf = (quiz_finished?1:0)+student.quiz_finished;
+        const qr = questions_rights+student.questions_rights;
+        const qw = questions_wrongs+student.questions_wrongs;
+        const response = await connection('students')
+        .where({registration})
+        .update({
+            quiz_finished:qf,
+            questions_rights:qr,
+            questions_wrongs:qw
+        });
+        return res.status(200).json({operationStatus:true});
+    }
 }
