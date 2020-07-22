a81367299983eed08f92c61754741b98cad302c2
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-07-22 19:42:30 -0300

    router config

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
-
+const Students = require('./Students');
+const Questions = require('./Questions');
 module.exports = (router)=>{
+    Students(router);
+    Questions(router);
     return router;
 }
