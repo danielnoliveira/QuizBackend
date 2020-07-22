f825839507e7c6ec8eaee31586fe3e4f02764ab3
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-07-22 18:11:47 -0300

    Funções de ranking terminadas

--- src/controllers/Students.js
+++ src/controllers/Students.js
@@ -1,13 +1,12 @@
 const connection = require('../database/config');
-
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const dbPath = path.resolve(__dirname+'/../src/database/', 'test.sqlite');
 module.exports = {
     create: async (req,res)=>{
         const {registration} = req.body;
         const [id] = await connection('students').insert({
             registration,
-            quiz_finished:0,
-            questions_rights:0,
-            questions_wrongs:0
         });
 
         return res.status(200).json({id});
@@ -19,13 +18,31 @@ module.exports = {
         const qf = (quiz_finished?1:0)+student.quiz_finished;
         const qr = questions_rights+student.questions_rights;
         const qw = questions_wrongs+student.questions_wrongs;
+        const addelo = qr-(qw*2);
         const response = await connection('students')
         .where({registration})
         .update({
             quiz_finished:qf,
             questions_rights:qr,
-            questions_wrongs:qw
+            questions_wrongs:qw,
+            elo:addelo
         });
-        return res.status(200).json({operationStatus:true});
+        return res.status(200).json({operationStatus:true,response});
+    },
+
+    ranking: async (req,res) => {
+        const results = await connection('students').select('registration','elo').orderBy('elo','desc');
+        return res.status(200).json(results);
+    },
+    myRanking: async (req,res)=>{
+      const {registration} = req.body;
+      const results = await connection('students').select('registration','elo').orderBy('elo','desc');
+      var i = -1;
+      for (i = 0; i < results.length; i++) {
+        if(results[i].registration===registration){
+          break;
+        }
+      }
+      return res.status(200).json({...results[i],position:i+1});
     }
 }
