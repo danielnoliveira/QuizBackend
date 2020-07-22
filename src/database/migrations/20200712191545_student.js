174f77b576589f0ae83456a176719ed6e31f7341
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-07-22 18:12:00 -0300

    Database update

--- src/database/migrations/20200712191545_student.js
+++ src/database/migrations/20200712191545_student.js
@@ -3,9 +3,10 @@ exports.up = async function(knex) {
   await knex.schema.createTable('students',(table)=>{
       table.increments('id').primary();
       table.string('registration').notNullable();
-      table.integer('quiz_finished').notNullable();
-      table.integer('questions_rights').notNullable();
-      table.integer('questions_wrongs').notNullable();
+      table.integer('quiz_finished').notNullable().defaultTo(0);
+      table.integer('questions_rights').notNullable().defaultTo(0);
+      table.integer('questions_wrongs').notNullable().defaultTo(0);
+      table.integer('elo').notNullable().defaultTo(0);
   });
 };
 
