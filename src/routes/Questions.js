2612e223752fc0b4ecff74f87acdc1175eb2c9ce
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2020-12-04 20:35:18 -0300

    Apagando arquivos inuteis

--- src/routes/Questions.js
+++
@@ -1,6 +0,0 @@
-const controller = require('../controllers/Questions');
-
-module.exports = (router)=>{
-    router.route('/questions/create').post(controller.create);
-    router.route('/questions/quiz').get(controller.getQuiz);
-};
--- src/routes/Students.js
+++
@@ -1,9 +0,0 @@
-const controller = require('../controllers/Students');
-
-module.exports = (router)=>{
-    router.route('/students/create').post(controller.create);
-    router.route('/students/update').post(controller.update);
-    router.route('/students/ranking').get(controller.ranking);
-    router.route('/students/myRanking').get(controller.myRanking);
-
-};
