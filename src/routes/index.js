f21a9509a1d56246679248ededd9172d116425b1
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2020-12-07 07:51:53 -0300

    update codigo base

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,3 @@
-const Students = require('./Students');
-const Questions = require('./Questions');
 module.exports = (router)=>{
-    Students(router);
-    Questions(router);
     return router;
 }
