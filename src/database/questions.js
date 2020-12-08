acc13dfd4c42264c63f844b9b39be94f420d5d58
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2020-12-08 19:53:13 -0300

    pesquisa de agrupamento de numero de alternativas das questões

--- src/database/questions.js
+++ src/database/questions.js
@@ -1,2 +1,12 @@
 const questions = require('./questions.json');
+// const optionsCount = {};
+// for (const q in questions) {
+//     const { options } = questions[q];
+//     if (optionsCount[options.length] !== undefined) {
+//         optionsCount[options.length] += 1;
+//     } else {
+//         optionsCount[options.length] = 1;
+//     }
+// }
+// console.table(optionsCount);
 module.exports = questions;
